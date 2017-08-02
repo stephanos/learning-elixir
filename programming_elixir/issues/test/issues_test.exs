0e8b4c8d80324fbba8bbfd60a55e6ae73988f17a
stephanos <github@stephanbehnke.de>
2017-08-02 08:40:33 -0400

    Programming Elixir: Issues #3

--- programming_elixir/issues/test/issues_test.exs
+++ programming_elixir/issues/test/issues_test.exs
@@ -2,7 +2,11 @@ defmodule ProgrammingElixirTest do
   use ExUnit.Case
   doctest Issues
 
-  import Issues.CLI, only: [ parse_args: 1 ]
+  import Issues.CLI, only: [ 
+    parse_args: 1,
+    sort_into_ascending_order: 1,
+    convert_to_list_of_maps: 1
+  ]
 
   test ":help returned by option parsing with -h and --help options" do
     assert parse_args(["-h",     "anything"]) == :help
@@ -15,5 +19,16 @@ defmodule ProgrammingElixirTest do
 
   test "count if defaulted if two values given" do
     assert parse_args(["user", "project"]) == { "user", "project", 4 }
+  end
+
+  test "sort ascending orders the correct way" do
+    result = sort_into_ascending_order(convert_to_list_of_maps([
+      [{"created_at", "c"}, {"other_data", "xxx"}],
+      [{"created_at", "a"}, {"other_data", "xxx"}],
+      [{"created_at", "b"}, {"other_data", "xxx"}]
+    ]))
+
+    issues = for issue <- result, do: issue["created_at"]
+    assert issues == ["a", "b", "c"]
   end
 end
